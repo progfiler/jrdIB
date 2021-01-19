89bdc57a80a9b72a61311f0777b79d688d5be670
Antoine Fissot <antoine@semifir.com>
2021-01-19 11:18:24 +0100

    Add coef def

--- JDRIB/Monde.cs
+++ JDRIB/Monde.cs
@@ -41,8 +41,8 @@ namespace JDRIB
             double coef = Utils.randomDouble();
             if (attacker.CoefAtk > coef)
             {
-                System.Console.WriteLine(opponent.Name + " va subir " + attacker.Damage + " dégats");
-                opponent.ReceiveDamage(attacker.Damage);
+                System.Console.WriteLine(opponent.Name + " va subir " + attacker.Damage + " dégats, mais " + opponent.Name + " peu encore se défendre !!!");
+                CaculateDefense(attacker, opponent);
                 System.Console.WriteLine("Suite à l'attaque, " + opponent.Name + " lui reste que " + opponent.Life + " de vie");
                 if (opponent.Life <= 0)
                 {
@@ -54,6 +54,22 @@ namespace JDRIB
                 System.Console.WriteLine("Le coeficient d'attaque de " + attacker.Name + " qui est de " + attacker.CoefAtk + " est inférieur à " + coef);
             }
 
+        }
+        private void CaculateDefense(Personnages attacker, Personnages opponent)
+        {
+            double coef = Utils.randomDouble();
+            System.Console.WriteLine(opponent.Name + " se prépare à défendre ....");
+            if (opponent.CoefDef > coef)
+            {
+                System.Console.WriteLine(opponent.Name + " c'est défendu !!!");
+                double reduceDamage = attacker.Damage * opponent.CoefDef;
+                attacker.Damage = attacker.Damage - Math.Round(reduceDamage);
+                System.Console.WriteLine("L'attaque a été réduite à " + attacker.Damage);
+            } else
+            {
+                System.Console.WriteLine(opponent.Name +  " n'a pas réussi à ce défendre !!");
+            }
+            opponent.ReceiveDamage(attacker.Damage);
         }
         private Personnages returnOppenent(Personnages currentPersonnage)
         {
